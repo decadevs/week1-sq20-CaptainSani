b3819db6db47600def3d1cfe2e9900560df7e09a
CaptainSani <sani.jdogo@gmail.com>
2024-02-07 07:20:18 -0800

    commit files in

--- SchoolManagementSys/src/main/java/TASK1/Teachers.java
+++ SchoolManagementSys/src/main/java/TASK1/Teachers.java
@@ -1,5 +1,12 @@
 package TASK1;
 
-public class Teachers{
+public class Teachers extends Staff{
+
+    public Teachers(String name, int age, int id) {
+        super(name, age, id);
+    }
+    public void teachCourse(){
+        System.out.println(" Teacher teach students based on class");
+    }
 
 }
--- SchoolManagementSys/src/main/java/TASK1/TestMain.java
+++ SchoolManagementSys/src/main/java/TASK1/TestMain.java
@@ -2,14 +2,35 @@ package TASK1;
 
 public class TestMain {
     public static void main(String[] args) {
-
-        Teachers teachers = new Teachers();
-        Students students = new Students("sani",true,true);
+        //Creating Objects for Staff and calling its method
         Staff staff = new Staff("ojay",30,001);
         Principal principal = new Principal("Goodstime",34,01);
-        Non_Academic_Staffs nonAcademicStaffs = new Non_Academic_Staffs("Abba", 27,70);
-        Classes classes = new Classes("primary 1A");
+
+        //Creating Objects for Students and calling its method
+        Students students = new Students("sani",true,true);
+        students.setMischievious(true);
+        students.isMischievious();
+        principal.expelStudent(students);
+
+        //Creating Objects for Applicants and calling its method
         Applicants applicants = new Applicants("Habib", 27);
+        applicants.getAge();
+        principal.admitStudents(applicants);
+
+        //Creating Objects for Non Academic staff and calling its method
+        Non_Academic_Staffs nonAcademicStaffs = new Non_Academic_Staffs("Abba", 27,70);
+        nonAcademicStaffs.role();
+
+        //Creating Objects for Teachers and calling its method
+        Teachers teachers = new Teachers("Eva Emma", 23, 9);
+        teachers.teachCourse();
+
+
+        //Creating Objects for Class and calling its method
+        Classes classes = new Classes("primary 1A");
+        classes.setClassName("Primary 4");
+        System.out.println(classes.getClassName());
+
 
 
     }
